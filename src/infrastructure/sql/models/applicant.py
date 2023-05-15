877a3f79434598094bfa3b146dae84b36e7a7932
Rodion <rodiononon@yandex.ru>
2023-05-16 00:36:20 +0300

    removed score from unique const

--- src/infrastructure/sql/models/applicant.py
+++ src/infrastructure/sql/models/applicant.py
@@ -16,7 +16,7 @@ class Applicant(Base):
     position = Column(SmallInteger, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
 
-    __table_args__ = (UniqueConstraint("snils", "code", "university", "score", "position", name="unique_applicant"),)
+    __table_args__ = (UniqueConstraint("snils", "code", "university", "position", name="unique_applicant"),)
 
     class Config:
         orm_mode = True
